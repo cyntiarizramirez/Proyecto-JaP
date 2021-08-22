8ed38581249d34fe8b33a50dcee24d11cbeb5ffc
enzoromero <88598504+enzoromero@users.noreply.github.com>
2021-08-22 19:43:12 -0300

    cambio1

--- js/products.js
+++ js/products.js
@@ -1,6 +1,17 @@
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultProducts){
+        if (resultProducts.status === "ok"){
+            console.log({
+                productos: resultProducts,
+            });
+        }
+
+    })
 
 });
