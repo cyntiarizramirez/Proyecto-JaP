8125b1c9130e80dfa168924abc319923de5d03a9
enzoromero <88598504+enzoromero@users.noreply.github.com>
2021-08-23 00:00:01 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -2,22 +2,20 @@ const showList = (productos) =>{
     const lista = document.createElement("ul");
     for (let producto of productos){
         const li_producto = document.createElement("li");
-
     }
     lista.appendChild(li_producto);
-    
+    document.body.appendChild(lista);
 }
-document.body.appendChild(lista);
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(PRODUCTS_URL).then(function (resultProducts){
-        if (resultProducts.status === "ok"){
-            showList(resultProducts.data);
+    getJSONData(PRODUCTS_URL).then(function (resultProductos){
+        if (resultProductos.status === "ok"){
+            showList(resultProductos.data);
             console.log({
-                productos: resultProducts.data,
+                productos: resultProductos.data,
             });
         }
 
