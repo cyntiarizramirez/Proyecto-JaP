6f245f20c4abc5e2b58db6d1f4676bf9ae5ff5a0
enzoromero <88598504+enzoromero@users.noreply.github.com>
2021-08-23 00:52:59 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -2,9 +2,10 @@ const showList = (productos) =>{
     const lista = document.createElement("ul");
     for (let producto of productos){
         const li_producto = document.createElement("li");
+        lista.appendChild(li_producto);
     }
     document.body.appendChild(lista);
-}
+};
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
