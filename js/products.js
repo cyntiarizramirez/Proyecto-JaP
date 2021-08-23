1af7869137e39e509c93beef27e8b1cd6054e894
enzoromero <88598504+enzoromero@users.noreply.github.com>
2021-08-22 22:37:26 -0300

    cambio2
    eff

--- js/products.js
+++ js/products.js
@@ -1,5 +1,12 @@
+const showList = (productos) =>{
+    const lista = document.createElement("ul");
+    for (let producto of productos){
+        const li_producto = document.createElement("li");
 
-
+    }
+    lista.appendChild(li_producto);
+    document.body.appendChild(lista);
+}
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -7,8 +14,9 @@
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCTS_URL).then(function (resultProducts){
         if (resultProducts.status === "ok"){
+            showList(resultProducts.data);
             console.log({
-                productos: resultProducts,
+                productos: resultProducts.data,
             });
         }
 
