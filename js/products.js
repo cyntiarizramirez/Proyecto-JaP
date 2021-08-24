e72ba1cb051a51a7e70b4ff98b669346e6ce0c19
cyntiariz <carolina.rizramirez@gmail.com>
2021-08-23 23:04:00 -0300

    edicion 2

--- js/products.js
+++ js/products.js
@@ -14,6 +14,7 @@ const showList = (productos) =>{
     document.body.appendChild(lista);
 };
 
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
