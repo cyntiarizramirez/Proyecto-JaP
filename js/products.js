b3d9e0edc906aa16b3e60bc10c3694c88423a1dc
cyntiariz <carolina.rizramirez@gmail.com>
2021-08-24 01:58:49 -0300

    Lista

--- js/products.js
+++ js/products.js
@@ -1,17 +1,30 @@
 const showList = (productos) =>{
     const lista = document.createElement("ul");
     for (let producto of productos){
+
+        const img = document.createElement("img");
+            img.src = producto.imgSrc;
+                lista.appendChild(img);
+
         const li_producto = document.createElement("li");
-        li_producto.appendChild(
-        document.createTextNode(`${producto.name}`));
-        const informacion = document.createElement("ul");
-        const li_informacion = document.createElement("li");
-        li_informacion.appendChild(
-            document.createTextNode(`${producto.description}`));
-        lista.appendChild(li_producto);
-        informacion.appendChild(li_informacion);
+            li_producto.appendChild(document.createTextNode(`${producto.name}`));
+                lista.appendChild(li_producto);
+
+        const li_description = document.createElement("ul");
+            li_description.appendChild(document.createTextNode(`${producto.description}`));
+                li_producto.appendChild(li_description);
+
+        
+        const li_cost = document.createElement("li");
+            li_cost.appendChild(document.createTextNode(` ${producto.currency} ${producto.cost}`));
+                li_description.appendChild(li_cost);
+
+        const li_soldCount = document.createElement("li");
+            li_soldCount.appendChild(document.createTextNode(`${producto.soldCount}`));
+                    li_description.appendChild(li_soldCount);
+
     }
-    document.body.appendChild(lista);
+document.body.appendChild(lista);
 };
 
 
