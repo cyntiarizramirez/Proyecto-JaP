af03a778c3ce3356a15687978776a24160044a7d
cyntiariz <carolina.rizramirez@gmail.com>
2021-11-03 22:37:34 -0300

    entrega5

--- js/cart.js
+++ js/cart.js
@@ -7,7 +7,36 @@ document.addEventListener("DOMContentLoaded", async () => {
     "https://japdevdep.github.io/ecommerce-api/cart/987.json"
     );
     articles = (await response.json()).articles;
-    console.log(articles)
+    
+    mostrarArticles(articles)
     
 });
+  const mostrarArticles = articles => {
+    console.log(articles)
+    articles.forEach(article =>{
+      console.log(article)
+      document.getElementById("imagen").innerHTML = article.src;
+      document.getElementById("nombre").innerHTML = article.name;
+      document.getElementById("cant").innerHTML = article.count;
+      document.getElementById("moneda").innerHTML = article.currency;
+      document.getElementById("precio").innerHTML = article.unitCost;
+      document.getElementById("subt").innerHTML = article.unitCost * article.count;
+
+      
+      
+    })};
+
+
+
+
+
+    /*let cantidad = article.count.value
+    const sumar = () => {
+      articles.forEach(article =>{
+    cantidad == article.count.value++;
+    })
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  document.getElementById("suma").addEventListener("click", sumar);})};*/
+
 
