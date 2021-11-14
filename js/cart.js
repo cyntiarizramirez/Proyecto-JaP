5e0ba68c9c947d53c41918736ffe26003bc61c3e
cyntiarizramirez <88387934+cyntiarizramirez@users.noreply.github.com>
2021-11-14 20:58:56 -0300

    Cambios del domingo 14

--- js/cart.js
+++ js/cart.js
@@ -7,36 +7,32 @@ document.addEventListener("DOMContentLoaded", async () => {
     "https://japdevdep.github.io/ecommerce-api/cart/987.json"
     );
     articles = (await response.json()).articles;
-    
     mostrarArticles(articles)
     
 });
+
   const mostrarArticles = articles => {
     console.log(articles)
     articles.forEach(article =>{
       console.log(article)
-      document.getElementById("imagen").innerHTML = article.src;
+      document.getElementById("imagen").innerHTML = `<img src="` + article.src + `" alt="Imagen del articulo">
+      `;
       document.getElementById("nombre").innerHTML = article.name;
-      document.getElementById("cant").innerHTML = article.count;
+      document.getElementById("cant").value = article.count;
       document.getElementById("moneda").innerHTML = article.currency;
       document.getElementById("precio").innerHTML = article.unitCost;
-      document.getElementById("subt").innerHTML = article.unitCost * article.count;
+      document.getElementById("subt").innerHTML = article.unitCost * document.getElementById("cant").value;
 
-      
-      
-    })};
+var cant = document.getElementById('cant');
+var updatesubt = function () {
+  document.getElementById("subt").innerHTML = article.unitCost * cant.value;
+}
 
+cant.addEventListener('input', function () {
+        updatesubt();
+    });
 
+})};
 
-
-
-    /*let cantidad = article.count.value
-    const sumar = () => {
-      articles.forEach(article =>{
-    cantidad == article.count.value++;
-    })
-
-document.addEventListener("DOMContentLoaded", function (e) {
-  document.getElementById("suma").addEventListener("click", sumar);})};*/
 
 
